24f6de036685d94f20b3f30adcd623a4468bac5d
ekko <1427841417@qq.com>
2026-07-19 19:51:39 +0800

    main.cpp

--- exercises/04_static/main.cpp
+++ exercises/04_static/main.cpp
@@ -5,15 +5,15 @@
 static int func(int param) {
     static int static_ = param;
     // std::cout << "static_ = " << static_ << std::endl;
-    return static_++;
+    return static_++;    // return static_++ 会先返回当前值，再将它加一，所以依次返回 5、6、7、8、9。
 }
 
 int main(int argc, char **argv) {
     // TODO: 将下列 `?` 替换为正确的数字
-    ASSERT(func(5) == ?, "static variable value incorrect");
-    ASSERT(func(4) == ?, "static variable value incorrect");
-    ASSERT(func(3) == ?, "static variable value incorrect");
-    ASSERT(func(2) == ?, "static variable value incorrect");
-    ASSERT(func(1) == ?, "static variable value incorrect");
+    ASSERT(func(5) == 5, "static variable value incorrect");
+    ASSERT(func(4) == 6, "static variable value incorrect");
+    ASSERT(func(3) == 7, "static variable value incorrect");
+    ASSERT(func(2) == 8, "static variable value incorrect");
+    ASSERT(func(1) == 9, "static variable value incorrect");
     return 0;
 }

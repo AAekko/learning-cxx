a5bb48b013ae39c7d46f9d383ef936b394f33836
ekko <1427841417@qq.com>
2026-07-19 20:02:08 +0800

    main.cpp

--- exercises/04_static/main.cpp
+++ exercises/04_static/main.cpp
@@ -2,6 +2,8 @@
 
 // READ: `static` 关键字 <https://zh.cppreference.com/w/cpp/language/storage_duration>
 // THINK: 这个函数的两个 `static` 各自的作用是什么？
+// 第一个`static`保证这个函数是当前文件的“私有函数”，不加`static`的话这个函数可能对其他 .cpp 文件开放。
+// 第二个`static`修饰的是函数内部的局部变量 static_ ，使得 static_ 成为局部静态变量 ，它具有两个重要特点：1.只在第一次执行到这行代码时初始化。 2.函数结束后不会被销毁，会记住上一次的值。
 static int func(int param) {
     static int static_ = param;
     // std::cout << "static_ = " << static_ << std::endl;

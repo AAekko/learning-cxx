925c08632b95d8b0659cccadcd38d57db6790ecd
ekko <1427841417@qq.com>
2026-07-23 17:24:28 +0800

    Update main.cpp

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -2,7 +2,13 @@
 
 // READ: 数组向指针退化 <https://zh.cppreference.com/w/cpp/language/array#%E6%95%B0%E7%BB%84%E5%88%B0%E6%8C%87%E9%92%88%E7%9A%84%E9%80%80%E5%8C%96>
 bool is_fibonacci(int *ptr, int len, int stride) {
-    ASSERT(len >= 3, "`len` should be at least 3");
+    ASSERT(len >= 3, "`len` should be at least 3");   //ASSERT 一般不是普通的流程控制。如果条件可能正常地不成立，通常应使用 if；ASSERT 更适合表示“按照程序设计，这个条件必须成立，否则说明调用方式或程序存在错误”。
+        for (int i = 0; i < len - 2; ++i) {
+        if (ptr[(i + 2) * stride] !=
+            ptr[i * stride] + ptr[(i + 1) * stride]) {
+            return false;
+        }
+    }
     // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
     // arr[i + 2] = arr[i] + arr[i + 1]
     return true;

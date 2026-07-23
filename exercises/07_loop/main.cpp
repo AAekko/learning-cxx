743fd369b7327a4322e85664892e43fe5690f5cb
ekko <1427841417@qq.com>
2026-07-23 16:21:51 +0800

    Update main.cpp

--- exercises/07_loop/main.cpp
+++ exercises/07_loop/main.cpp
@@ -6,9 +6,10 @@
 static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
     static unsigned long long cache[96]{0, 1};//使用 static 声明的（局部变量）只初始化一次，并且会一直存在到程序结束。普通局部变量则会在每次调用函数时重新创建。
-    static int cached = 2;//因为循环中使用了 cached，所以要先声明。cached 用来记录“已经计算到哪里”，而 static 让这个进度在多次函数调用之间保留下来
+    static int cached = 2;//因为循环中使用了 cached，所以要先声明。cached 用来记录“已经计算到哪里”，而 static 让这个进度在多次函数调用之间保留下来。  缓存cached也是有意义的，可以减少不必要的循环，因为前面的cache已经算过了。
     //第一次调用 fibonacci()，初始化 cache 和 cached，函数结束后，变量仍然存在；第二次调用 fibonacci()，不再初始化，继续使用上次留下的值；第三次调用 fibonacci()，仍然使用之前留下的值，程序结束，cache 和 cached 的生命周期结束。
     // TODO: 设置正确的循环条件
+    //for循环：for (初始化; 继续循环的条件; 每轮“结束后”的操作)。下面那句；前面是空的表示没有初始化。
     for (; cached <= i; ++cached) {
         cache[cached] = cache[cached - 1] + cache[cached - 2];
     }
